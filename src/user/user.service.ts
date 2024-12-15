d9c435438263894c5c1e1fcf4f80fa02ea7d4e8f
Abhishek p p <abhishekkanichery@gmail.com>
2024-12-15 13:01:15 +0530

    added user services of user crud

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  createUsers(userDetails: any) {
+    console.log('create users');
+    console.log(userDetails);
+    return userDetails;
+  }
+
+  gerAllUsers(searchFilters: any) {
+    console.log('get all users');
+    console.log(searchFilters);
+    const user = [];
+    user.push({
+      id: 1,
+      name: 'abhi',
+    });
+    return user;
+  }
+
+  getUserProfile(userId: string) {
+    console.log('get user profile');
+    const user = {
+      id: userId,
+      name: 'abhi',
+    };
+    return user;
+  }
+
+  updateUser(userId: string, updateData: any) {
+    console.log('user update');
+    console.log({
+      userId,
+      updateData,
+    });
+  }
+
+  deleteUser(userId: string) {
+    console.log('Delete user');
+    console.log(userId);
+  }
+}
