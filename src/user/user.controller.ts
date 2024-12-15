aa51b97597fcbe2f0643fd98a92c278eab2b36f6
Abhishek p p <abhishekkanichery@gmail.com>
2024-12-15 13:15:22 +0530

    users services inject into the user controllers

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,36 +8,39 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private userServices: UserService) {}
   @Post()
   createUsers(@Body() userDto: any): string {
-    console.log(userDto);
+    this.userServices.createUsers(userDto);
     return 'User created';
   }
 
   @Get()
-  gerAllUsers(@Query() searchFilters: any): string {
-    console.log(searchFilters);
-    return 'Hello User!';
+  gerAllUsers(@Query() searchFilters: any): any {
+    const users = this.userServices.gerAllUsers(searchFilters);
+    console.log(users);
+    return users;
   }
 
   @Get(':id')
-  getUserProfile(@Param('id') id: string): string {
-    return `User Profile of ${id}`;
+  getUserProfile(@Param('id') id: string): any {
+    const userProfile = this.userServices.getUserProfile(id);
+    return userProfile;
   }
 
   @Patch(':id')
   updateUser(@Param('id') id: string, @Body() updateDto: any): string {
-    console.log(`User update with id: ${id}`);
-    console.log(updateDto);
+    this.userServices.updateUser(id, updateDto);
     return `User update: ${id}`;
   }
 
   @Delete('id')
   deleteUser(@Param('id') id: string): string {
-    console.log(`User with ${id} deleted`);
+    this.userServices.deleteUser(id);
     return `User deleted: ${id}`;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,41 +3,61 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UserService {
   createUsers(userDetails: any) {
-    console.log('create users');
-    console.log(userDetails);
-    return userDetails;
+    try {
+      console.log('create user');
+      console.log(userDetails);
+      return userDetails;
+    } catch (error) {
+      throw error;
+    }
   }
 
   gerAllUsers(searchFilters: any) {
-    console.log('get all users');
-    console.log(searchFilters);
-    const user = [];
-    user.push({
-      id: 1,
-      name: 'abhi',
-    });
-    return user;
+    try {
+      console.log('get all users');
+      console.log(searchFilters);
+      const user = [];
+      user.push({
+        id: 1,
+        name: 'abhi',
+      });
+      return user;
+    } catch (error) {
+      throw error;
+    }
   }
 
   getUserProfile(userId: string) {
-    console.log('get user profile');
-    const user = {
-      id: userId,
-      name: 'abhi',
-    };
-    return user;
+    try {
+      console.log('get user profile');
+      const user = {
+        id: userId,
+        name: 'abhi',
+      };
+      return user;
+    } catch (error) {
+      throw error;
+    }
   }
 
   updateUser(userId: string, updateData: any) {
-    console.log('user update');
-    console.log({
-      userId,
-      updateData,
-    });
+    try {
+      console.log('user update');
+      console.log({
+        userId,
+        updateData,
+      });
+    } catch (error) {
+      throw error;
+    }
   }
 
   deleteUser(userId: string) {
-    console.log('Delete user');
-    console.log(userId);
+    try {
+      console.log('Delete user');
+      console.log(userId);
+    } catch (error) {
+      throw error;
+    }
   }
 }
