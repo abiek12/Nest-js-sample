b778d35846ea96101d5d51575da9fe2798c677a0
Abhishek p p <abhishekkanichery@gmail.com>
2024-12-17 07:59:13 +0530

    register the auth middleware for user routes of all requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthMiddleware } from './auth/auth.middleware';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes({ path: 'user', method: RequestMethod.ALL });
+  }
+}
