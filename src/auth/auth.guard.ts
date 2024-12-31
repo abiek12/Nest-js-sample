e94f116c3519d3782c77bc1706800b9fe9eaf8e3
Abhishek p p <abhishekkanichery@gmail.com>
2024-12-31 08:16:55 +0530

    feat: role guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -2,10 +2,11 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class RoleGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    console.log(context);
     return true;
   }
 }
