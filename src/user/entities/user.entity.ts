dafc28c4e51a5831917cbd518603911617ca1588
Abhishek p p <abhishekkanichery@gmail.com>
2025-01-08 23:01:09 +0530

    fix some issues

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { GenderEnum } from '../enums/user.enums';
 
-@Entity()
+@Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn()
   userId: number;
