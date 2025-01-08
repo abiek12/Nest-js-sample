7e91c8e117a1847b16e51312f58587412477d72c
Abhishek p p <abhishekkanichery@gmail.com>
2025-01-08 08:10:01 +0530

    formated and linted

--- src/common/datasource/typeorm.module.ts
+++ src/common/datasource/typeorm.module.ts
@@ -1,38 +1,38 @@
 import { Global, Logger, Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 
 @Global() // makes the module available globally for other modules once imported in the app modules
 @Module({
-    imports: [],
-    providers: [
-        {
-            provide: DataSource, // add the datasource as a provider
-            inject: [ConfigService],
-            useFactory: async (configService: ConfigService) => {
-                const logger = new Logger();
-                // using the factory function to create the datasource instance
-                try {
-                    const datasource = new DataSource({
-                        type: 'postgres',
-                        host: configService.get<string>('DB_HOST'),
-                        port: configService.get<number>('DB_PORT') ?? 5433,
-                        username: configService.get<string>('DB_USER'),
-                        password: configService.get<string>('DB_PASSWORD'),
-                        database: configService.get<string>('DB_NAME'),
-                        synchronize: false,
-                        entities: [`${__dirname}/../**/**.entity{.ts,.js}`], // this will automatically load all entity file in the src folder
-                    });
-                    await datasource.initialize();
-                    logger.log("Database connected successfully");
-                    return datasource;
-                } catch (error) {
-                    logger.error("Error connecting to database", error);
-                    throw error;
-                }
-            }
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: DataSource, // add the datasource as a provider
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const logger = new Logger();
+        // using the factory function to create the datasource instance
+        try {
+          const datasource = new DataSource({
+            type: 'postgres',
+            host: configService.get<string>('DB_HOST'),
+            port: configService.get<number>('DB_PORT') ?? 5433,
+            username: configService.get<string>('DB_USER'),
+            password: configService.get<string>('DB_PASSWORD'),
+            database: configService.get<string>('DB_NAME'),
+            synchronize: false,
+            entities: [`${__dirname}/../**/**.entity{.ts,.js}`], // this will automatically load all entity file in the src folder
+          });
+          await datasource.initialize();
+          logger.log('Database connected successfully');
+          return datasource;
+        } catch (error) {
+          logger.error('Error connecting to database', error);
+          throw error;
         }
-    ],
-    exports: [DataSource]
+      },
+    },
+  ],
+  exports: [DataSource],
 })
 export class TypeormModule {}
