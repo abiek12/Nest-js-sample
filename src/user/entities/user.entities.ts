a9f3bc0b574171d337f6d7df67c12349c63ba2e3
Abhishek p p <abhishekkanichery@gmail.com>
2025-01-08 08:24:00 +0530

    feat: user entities

---
+++ src/user/entities/user.entities.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { GenderEnum } from "../enums/user.enums";
+
+@Entity()
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    userId: number
+
+    @Column({type: 'varchar', name: 'name', nullable: false})
+    name: string
+
+    @Column({type: 'varchar', name: 'phone', nullable: false})
+    phone: string
+
+    @Column({type: 'varchar', name: 'password', nullable: false})
+    password: string
+
+    @Column({type: 'enum', name: 'gender', nullable: true, enum: GenderEnum})
+    gender: string
+}
