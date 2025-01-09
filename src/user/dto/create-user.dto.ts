0ac3d978741df97952be0f1cd6c541fd13429e11
Abhishek p p <abhishekkanichery@gmail.com>
2025-01-09 22:52:51 +0530

    updates the class validator

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,15 +1,23 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, Max, Min } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
+  @Min(3)
+  @Max(20)
   @IsString()
   name: string;
 
+  @IsNotEmpty()
   @IsString()
+  @Min(5)
   password: string;
 
+  @IsNotEmpty()
   @IsString()
+  @Max(12)
   phone: string;
 
+  @IsOptional()
   @IsString()
   gender: string;
 }
