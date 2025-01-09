abe44f2f9a9d8a28f3b5f387b1eed77a25d5c6ee
Abhishek p p <abhishekkanichery@gmail.com>
2025-01-09 22:53:37 +0530

    updates the class validator

--- src/user/dto/filter-user.dto.ts
+++ src/user/dto/filter-user.dto.ts
@@ -1,12 +1,15 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class UserFilterDto {
+  @IsOptional()
   @IsNumber()
   page: number;
 
+  @IsOptional()
   @IsNumber()
   size: number;
 
+  @IsOptional()
   @IsString()
   name: string;
 }
